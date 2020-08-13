4538212fb6ad43f9265495dcc92df08ea15406dc
PutridFarm <2357827+PutridFarm@users.noreply.github.com>
2020-08-13 16:06:10 -0400

    Practice crawler working
    Got my first crawler to work, saving changes for reference. Need to incorperate into my app now.

--- retro-collection/scripts/crawler/crawler/spiders/games.py
+++ retro-collection/scripts/crawler/crawler/spiders/games.py
@@ -7,4 +7,12 @@ class GamesSpider(scrapy.Spider):
     start_urls = ['https://gamevaluenow.com/super-nintendo/']
 
     def parse(self, response):
-        pass
+        games_table = response.xpath('//table[@id="platformGameListTable"]')
+        rows = games_table.xpath('//tr')
+        # Helpful debug: print("1 row", rows[1].xpath('.//td').extract())
+        for row in rows[1:]:  # ignore first thead row
+            title = row.xpath('.//td/a/text()').extract()
+            loose_price = row.xpath('.//td[2]/text()').extract()
+            complete_price = row.xpath('./td[3]/text()').extract()
+            new_price = row.xpath('./td[4]/text()').extract()
+            yield {'Title': title, 'Loose Price': loose_price, 'Complete Price': complete_price, 'New Price': new_price}

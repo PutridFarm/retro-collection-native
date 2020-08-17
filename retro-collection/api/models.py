5fdafb5e1380ec38e1e3c547678948067f386247
PutridFarm <2357827+PutridFarm@users.noreply.github.com>
2020-08-17 12:39:24 -0400

    Fix add game
    Fixing regression introduced with last change set

--- retro-collection/api/models.py
+++ retro-collection/api/models.py
@@ -3,7 +3,7 @@ from . import db
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
-    consoleId = db.Column(db.String(50), db.ForeignKey('console.consoleId'))
+    consoleId = db.Column(db.String(50), db.ForeignKey('console.id'))
     text = db.Column(db.Text, nullable=False, default="")
 
 class Console(db.Model):

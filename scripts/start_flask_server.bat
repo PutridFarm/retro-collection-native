6574b36e62b409ebad842268f0171b209110225c
PutridFarm <2357827+PutridFarm@users.noreply.github.com>
2020-10-15 15:32:11 -0400

    Removed crawler from scripts, updated start scripts
    Made some changes when trying to set up after migration from other repo

--- scripts/start_flask_server.bat
+++ scripts/start_flask_server.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO Starting retro-collection back-end api...
-cd "C:\work\React\galactic-core\retro-collection\api"
+cd "C:\work\retro-collection-native"
 pipenv run flask run -h 192.168.0.134 -p 5000
 PAUSE
